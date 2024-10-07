d5d25be5d883be3ed74dd635040838373d5be021
DenisDovz <killxvzxl@gmail.com>
2024-10-08 01:17:38 +0300

    Add files via upload

---
+++ Denis PR 1.py
@@ -0,0 +1,14 @@
+hello = "Hello World"
+name = "Denys"
+prizv = "Dovzhik"
+age = 16
+
+print(hello)
+print(name)
+print(prizv)
+print(age)
+
+print(type(hello))
+print(type(name))
+print(type(prizv))
+print(type(age))
